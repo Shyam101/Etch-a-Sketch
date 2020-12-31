287771e074ed1cb72adbcaec583550b39fed33ee
Shyam <iamsv345@gmail.com>
2020-12-31 23:58:36 +0530

    Added clear button

--- main.js
+++ main.js
@@ -1,4 +1,49 @@
+const container = document.querySelector('.container');
+const clear = document.querySelector('#btn');
+
+let side = container.clientHeight;
+
+console.log(side);
+
+container.style.height =side;
+container.style.width = side;
+
+var n = prompt("enter number of box for each side");
+adddivs(n);
+
+
 //functions
+
+clear.addEventListener('click',function(){
+    container.innerHTML = "";
+    n = prompt("enter number of box for each side");
+    adddivs(n);
+})
+
+function adddivs(n)
+{
+    newdiv = document.createElement('div');
+    newdiv.style.cssText = "display:flex; flex-direction: row;"; 
+
+    temdiv = document.createElement('div');
+    temdiv.classList.add('mini');
+    var mside = side/n;
+    temdiv.style.height=mside + "px";
+    temdiv.style.width=mside + "px";
+
+
+    for(let i=0;i<n;++i)
+        newdiv.appendChild(temdiv.cloneNode(true));
+
+    for(let i=0;i<n;++i)
+        container.appendChild(newdiv.cloneNode(true));
+
+    const cells = document.querySelectorAll('.mini');
+
+    cells.forEach((div)=>{
+    div.addEventListener('mouseover',changeColor);
+    })
+}
 
 function random()
 {
@@ -13,42 +58,3 @@ function changeColor()
 {
     this.style.background = randomrgb();
 }
-
-//
-const container = document.querySelector('.container')
-
-let side = container.clientHeight;
-
-console.log(side);
-
-container.style.height =side;
-container.style.width = side;
-
-var n = prompt("enter number of box for each side");
-var mside = side/n;
-
-console.log(mside);
-
-
-newdiv = document.createElement('div');
-newdiv.style.cssText = "display:flex; flex-direction: row;"; 
-newdiv.style.height=mside + "px";
-newdiv.style.width=side + "px";
-
-temdiv = document.createElement('div');
-temdiv.classList.add('mini');
-temdiv.style.height=mside + "px";
-temdiv.style.width=mside + "px";
-
-
-for(let i=0;i<n;++i)
-    newdiv.appendChild(temdiv.cloneNode(true));
-
-for(let i=0;i<n;++i)
-    container.appendChild(newdiv.cloneNode(true));
-
-const cells = document.querySelectorAll('.mini');
-
-cells.forEach((div)=>{
-    div.addEventListener('mouseover',changeColor);
-})
